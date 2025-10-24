e11f39847aa4ec28e657f3ee3da4be7f34f35b47
Yuri Eduardo Lencio <yurilencio@ciandt.com>
2025-10-24 09:51:58 -0300

    adicionando codigo base

---
+++ js/function.js
@@ -0,0 +1,9 @@
+console.log("Olá mundo")
+
+const sprites = new Image();
+sprites.src = 'css/sprites.png'; 
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+contexto.drawImage();
